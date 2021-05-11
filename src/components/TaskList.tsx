3a612ab9fd7453b3f4891eb72aff633680c2cfc0
Claybson <claybson@cartello.com.br>
2021-05-10 22:01:16 -0300

    finalizando desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,28 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim()){
+      const newTask = {
+        'id': Math.random(),
+        'title': newTaskTitle,
+        'isComplete': false
+      }
+      setTasks(arrayTasks => [...arrayTasks, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const arrayTasks = tasks.map( task => id == task.id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    }: task );
+    setTasks(arrayTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const arrayTasks = tasks.filter(task => task.id !== id);
+    setTasks(arrayTasks);
   }
 
   return (
